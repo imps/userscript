5743fd28578143c529bc14d2d07ebd2ac9c953e6
aszlig <aszlig@redmoonstudios.org>
2012-05-08 05:57:44 +0200

    Add new Constants class.

---
+++ userscript/Constants.hx
@@ -0,0 +1,12 @@
+package userscript;
+
+class Constants
+{
+    public static var USERSCRIPT_VARIABLES = [
+        "name", "namespace", "version", "description", "downloadURL", "exclude",
+        "icon", "include", "match", "require", "resource", "run-at", "unwrap",
+        "updateURL"
+    ];
+
+    public static var CODE_PLACEHOLDER = "####!\\CODE_HERE!\\####";
+}
